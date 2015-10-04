47c77078a27e04838c54a7182609b8100c2a3119
Christian Boutin <christian.boutin@gmail.com>
2015-10-04 09:30:55 -0400

    First commit

---
+++ CopyDir.py
@@ -0,0 +1,62 @@
+The MIT License (MIT)
+
+Copyright (c) 2015 Exequor Studios Inc. (exequor)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+import os
+import shutil
+import hashlib
+
+def sha1OfFile(filepath):
+    import hashlib
+    with open(filepath, 'rb') as f:
+        return hashlib.sha1(f.read()).hexdigest()
+
+def copydir(sourcePath, destinationPath):
+    #onlydirs = [f for f in os.listdir(mypath) if not os.path.isfile(os.path.join(sourcePath,f))]
+    #print(onlydirs)
+    #for onedir in onlydirs:
+
+        #copydir(mypath+"/"+onedir)
+
+    onlyfiles = [f for f in os.listdir(sourcePath)]
+
+    for onefile in onlyfiles:
+        srcfile = sourcePath+"/"+onefile
+        dstfile = destinationPath+"/"+onefile
+
+        if os.path.isdir(srcfile):
+            if not os.path.isdir(dstfile):
+                os.makedirs(dstfile)
+                print("Making Dir:",dstfile)
+            copydir(srcfile, dstfile)
+        elif os.path.isfile(srcfile):
+            if os.path.isfile(dstfile):
+                if os.path.getmtime(srcfile) == os.path.getmtime(dstfile):
+                    print ("DATE EQUAL: "+srcfile)
+                elif sha1OfFile(srcfile) == sha1OfFile(dstfile):
+                    print ("DATE DIFFERENT, SIG EQUAL: "+srcfile)
+                    os.utime(dstfile, (-1,os.path.getmtime(srcfile)))
+                else:
+                    print ("DATE DIFFERENT, SIG DIFFERENT, COPY: "+srcfile)
+                    shutil.copyfile(srcfile, dstfile)
+            else:
+                shutil.copyfile(srcfile, dstfile)
+                print ("UNSCHWING!")
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CopyDir
 A small toolset to copy a folder from a synched (dropbox type) folder to one linked to a git repo.
+
+in Settings.py, set gitPath to the root path of where you keep your git-synched folders, syncPath to the shared folder, activeProjects is an array of folders that you want to keep in sync.
+
+After that, run CopyToGit or CopyFromGit depending.  The copy algorithm tests for things like date and signature before copying, so as to prevent accidental erasure.  Might still have bugs, so use at own risks.  Refer to CopyDir.git and check the code to see all the checks made.
+
+This is useful if you collaborate with someone who cannot use Git.  You can sync things with them that way.
+
+chris@exequor.com
